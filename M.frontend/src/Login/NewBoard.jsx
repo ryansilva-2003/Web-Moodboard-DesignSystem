33777ca67015b193cb2f7945bc0503662563c649
Ryan <ryansilvadeoliveira53@gmail.com>
2025-11-29 19:05:39 -0300

    Melhoria da interface do home

--- M.frontend/src/Login/NewBoard.jsx
+++ M.frontend/src/Login/NewBoard.jsx
@@ -11,6 +11,8 @@ export default function NewBoard(){
     const [boards, setBoards] = useState([]);
     const token = localStorage.getItem("token");
     const [editId, setEditId] = useState(null);
+    const [previewImage, setPreviewImage] = useState(null);
+    const [openModalDelete, setOpenModalDelete] = useState(false);
     console.log("TOKEN:", token);
 
     useEffect(() => {
@@ -29,11 +31,19 @@ export default function NewBoard(){
         carregarBoards();
     }, []);
 
+            const limparCampos = () => {
+            setEditId(null);
+            setTitle("");
+            setDescription("");
+            setImage(null);
+            setColors("#ffffff");
+        };
+
     const handleBoardSubmit = async (event) => {
         event.preventDefault();
 
         if (!title) {
-            alert("Título é obrigatórios.");
+            alert("Título é obrigatório.");
              return;
         }
 
@@ -117,23 +127,25 @@ return (
         {IsModalOpen && (
             <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-20">
 
-                <div className="bg-[#1f1f1f] text-white p-6 rounded-xl w-[1100px] h-[700px] shadow-xl flex overflow-hidden">
+                <div className="bg-[#1f1f1f] text-white p-6 rounded-xl w-[1100px] h-[800px] shadow-xl flex overflow-hidden">
+
 
                     {/* lado esquerdo */}
                     <div className="w-1/2 flex flex-col justify-center mr-10">
 
-                        <div className="flex-1 flex items-center justify-center p-5">
+                    <h1 className="text-white text-2xl underline underline-offset-5 decoration-orange-400">{editId ? "EDITAR BOARD" : "CRIAR BOARD"}</h1>
+                        <div className="flex-1 flex items-center justify-center p-5 max-h-[650px]">
                             {image ? (
                                 typeof image === "string" ? (
                                     <img 
                                     src={`http://localhost:4000/uploads/${image}`}
                                     alt="Preview"
-                                    className="object-contain"/>
+                                    className="object-contain max-h-full max-w-full"/>
                                 ) : (
                                 <img 
                                     src={URL.createObjectURL(image)} 
                                     alt="Preview" 
-                                    className="object-contain"/>
+                                    className="object-contain max-w-full max-h-full"/>
                                 )
                             ) : (
                                 <p className="text-gray-600 bg-[#a0a0a0] w-full h-full flex items-center justify-center">Nenhuma imagem selecionada<span class="material-symbols-outlined">hide_image</span></p>
@@ -160,7 +172,7 @@ return (
                     <form className="w-1/2 p-3 flex flex-col justify-between" onSubmit={handleBoardSubmit}>
 
                         <div>
-                            <label className="block font-semibold mb-1">TÍTULO</label>
+                            <label className="block font-semibold mb-1 text-xl">TÍTULO</label>
                             <input 
                                 type="text" 
                                 value={title} 
@@ -173,11 +185,11 @@ return (
                         <hr className="border-t border-orange-500 mt-2 w-full" />
 
                         <div>
-                            <label className="block font-semibold mb-1">DESCRIÇÃO</label>
+                            <label className="block font-semibold mb-1 text-xl">DESCRIÇÃO</label>
                             <textarea 
                                 value={description} 
                                 onChange={(e) => setDescription(e.target.value)}
-                                className="w-full h-50 py-2 px-3 border rounded" 
+                                className="w-full h-60 py-2 px-3 border rounded" 
                                 placeholder="Ex: Arte que fiz para representar o verão que tive..."
                             />
                         </div>
@@ -185,22 +197,24 @@ return (
                         <hr className="border-t border-orange-500 w-full" />
 
                         <div>
-                            <label className="block font-semibold mb-1">COR DO BOARD</label>
+                            <label className="block font-semibold mb-1 text-xl">COR DO BOARD</label>
                             
                             <input 
                                 type="color" 
                                 value={colors} 
                                 onChange={(e) => setColors(e.target.value)} 
-                                className="w-full h-15 cursor-pointer"/>
+                                className="w-full h-16 cursor-pointer"/>
                         </div>
 
                         <hr className="border-t border-orange-500 w-full" />
 
                         <button 
                             type="submit" 
-                            className="cursor-pointer w-full bg-blue-600 mt-6 text-white py-2 rounded-lg hover:bg-blue-700 transition">CRIAR BOARD</button>
+                            className="cursor-pointer w-full bg-blue-600 mt-6 text-white py-3 rounded-lg hover:bg-blue-700 transition">{editId ? "SALVAR ALTERAÇÕES" : "CRIAR NOVO BOARD"}</button>
 
-                            <button type="button" onClick={() => setIsModalOpen(false)} className="cursor-pointer w-full bg-gray-600 mt-1 text-white py-2 rounded-lg hover:bg-gray-700 transition">FECHAR</button>
+                            <button type="s" className="cursor-pointer w-full bg-gray-500 mt-3 py-2 rounded-lg hover:bg-red-700 transition flex justify-center"><span class="material-symbols-outlined">delete</span>Deletar</button>
+
+                            <button type="button" onClick={() => { limparCampos(); setIsModalOpen(false)}}className="cursor-pointer w-full bg-gray-600 mt-1 text-white py-2 rounded-lg hover:bg-gray-700 transition">Cancelar</button>
 
                     </form>
 
@@ -209,36 +223,47 @@ return (
             </div>
         )}
 
-<div className="mt-10 grid grid-cols-3 gap-12 ">
-    {boards.map(board => (
-        <div key={board.id} className="bg-[#1f1f1f] p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-3 transition-all duration-300 cursor-pointer border border-gray-700 mb-10">
-            {board.image && (
+            <div className="mt-10 columns-5 gap-12">
+                {boards.map(board => (
+                <div key={board.id} className="bg-[#404040] p-4 rounded-2xl shadow-lg hover:shadow-2xl hover:-translate-y-3 transition-all duration-300 cursor-pointer border border-gray-700 mb-6 break-inside-avoid">
+                {board.image && (
                 <img 
                     src={`http://localhost:4000/uploads/${board.image}`} 
                     alt="imagem" 
-                    className="w-full h-70 object-contain rounded-xl mb-3"
-                />
-            )}
+                    className="max-w-full h-auto object-cover rounded-xl mb-3 cursor-pointer"
+                    onClick={() => {
+            if (!board.image) return;
+                const src = typeof board.image === "string" ? `http://localhost:4000/uploads/${board.image}` : URL.createObjectURL(board.image);
+            setPreviewImage(src);
+        }}
+
+                    
+            />)}
             <h2 className="text-xl font-semibold text-white">{board.title}</h2>
             {board.description && <p className="text-gray-400 text-sm mt-1">{board.description}</p>}
             {board.colors && (
                 <div 
-                    className="w-full h-4 rounded mt-5" 
+                    className="w-full h-6 rounded mt-5" 
                     style={{ backgroundColor: board.colors }}/>
+                )}
 
-                    
-            )}
-
-        <button 
+            <button 
             onClick={() => handleEdit(board)}
-            className="bg-blue-500 text-white px-2 py-1 mt-5 rounded cursor-pointer"
-        >
-            Editar
-        </button>
-            
+            className="px-2 mt-2 cursor-pointer text-2xl text-white hover:bg-gray-600 rounded-xl">...
+            </button>
         </div>
-    ))}
+        ))}
 </div>
 
+{previewImage && (
+  <div 
+    className="fixed inset-0 bg-black/90 flex items-center justify-center cursor-pointer"
+    onClick={() => setPreviewImage(null)}
+  >
+    <img src={previewImage} alt="Preview Grande" className="max-h-[90%] max-w-[90%] object-contain rounded-lg shadow-xl"/>
+  </div>
+)}
+
     </div>
-)};
+);
+}
